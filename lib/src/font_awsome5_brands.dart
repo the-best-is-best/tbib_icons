c390664adf9fa92cb0664fbeaffe42a06713ed50
Michelle Raouf <michelle.raouf@outlook.com>
2021-08-27 21:46:13 +0200

    Feather icons added

--- lib/src/font_awsome5_brands.dart
+++ lib/src/font_awsome5_brands.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class FontAwsome5Brands {
   FontAwsome5Brands._();
--- lib/src/font_awsome5_solid.dart
+++ lib/src/font_awsome5_solid.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class FontAwsome5Solid {
   FontAwsome5Solid._();
